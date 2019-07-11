8a31e3652d5d8b1435a0c6c7c682304e23eaa014
zxc471313304 <52726197+zxc471313304@users.noreply.github.com>
2019-07-11 10:39:05 +0800

    修改脚本

--- Demo1/Assets/Scripts/AI.cs
+++ Demo1/Assets/Scripts/AI.cs
@@ -1,13 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+//状态
+public enum EnemyState
+{
+    idle,
+    run,
+    attack
+}
 public class AI : MonoBehaviour
 {
+    //状态
+    public EnemyState CurrentState = EnemyState.idle;
+    //动画控制器
+    private Animation ani;
+    //玩家
+    private Transform player;
+    //导航代理
+    private NavMeshAgent agent; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ani = GetComponent<Animation>();
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
