42815ed679aa2cde248ae862f9d352087bda5a12
EmperorMoMo <52726797+EmperorMoMo@users.noreply.github.com>
2019-07-11 09:58:37 +0800

    编写脚本

--- Demo1/Assets/Scripts/PlayerInput.cs
+++ Demo1/Assets/Scripts/PlayerInput.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+    //Variable  变数变量区
+    public string keyUp = "w";
+    public string keyDown = "s";
+    public string keyLeft = "a";
+    public string keyRight = "d";
+
+    public float Dup;
+    public float Dright;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,6 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Dup = (Input.GetKey(keyUp) ? 1.0f : 0) - (Input.GetKey(keyDown) ? 1.0f : 0);    //  ()?():()
     }
 }
