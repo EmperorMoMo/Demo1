87a76eb231d89164c4627459cc8973026ad55493
AshesFeathers <1206996078@qq.com>
2019-07-13 13:09:12 +0800

    优化技能冷却代码

--- Demo1/Assets/Scripts/Shi/SkillItem.cs
+++ Demo1/Assets/Scripts/Shi/SkillItem.cs
@@ -24,11 +24,11 @@ public class SkillItem : MonoBehaviour {
         if (isStartTimer) { //如果计时器开始执行
             timer += Time.deltaTime; //计时器的时间开始往上累加
             filledImage.fillAmount = (coldTime - timer) / coldTime; //武器的阴影图按照比例增加
-        }
-        if (timer >= coldTime) { //计时器的时间超过了技能冷却时间
-            filledImage.fillAmount = 0; //武器的阴影图隐藏
-            timer = 0; //计时器归零
-            isStartTimer = false; //是否开始计算时间
+            if (timer >= coldTime) { //计时器的时间超过了技能冷却时间
+                filledImage.fillAmount = 0; //武器的阴影图隐藏
+                timer = 0; //计时器归零
+                isStartTimer = false; //是否开始计算时间
+            }
         }
     }
 
