51b574b988e70779b7a7b22e0791c1bdf92d0f41
EmperorMoMo <52726797+EmperorMoMo@users.noreply.github.com>
2019-07-16 12:11:47 +0800

    添加盾牌

---
+++ Demo1/Assets/Scripts/Han/LeftArmAnimFix.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftArmAnimFix : MonoBehaviour
+{
+
+    private Animator anim;
+
+    public Vector3 a;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    void OnAnimatorIK()
+    {
+        Transform leftLowerArm = anim.GetBoneTransform(HumanBodyBones.LeftLowerArm);//取得模型上的左下臂的骨头
+        leftLowerArm.localEulerAngles += 0.75f * a;//设置手臂旋转角度
+        //通过SetBoneLocalRotation(要旋转的骨头，旋转角度的四元素)方法让手臂旋转        
+        anim.SetBoneLocalRotation(HumanBodyBones.LeftLowerArm, Quaternion.Euler(leftLowerArm.localEulerAngles));
+    }
+
+}
